11750248e01555c6885e0733532b5fc35943fe3e
Daniel Vicario <danigv.email@gmail.com>
2019-07-31 12:10:26 +0200

    added all

--- src/globe/scene.js
+++ src/globe/scene.js
@@ -6,12 +6,43 @@ export const rootMesh = new THREE.Mesh(new THREE.Geometry())
 export function init (container) {
   const width = container.offsetWidth || window.innerWidth
   const height = container.offsetHeight || window.innerHeight
-  const camera = new THREE.PerspectiveCamera(30, width / height, 1, 10000)
+  const camera = new THREE.PerspectiveCamera(30, width / height, 1, 30000)
   const renderer = new THREE.WebGLRenderer({ antialias: true })
   const controls = new OrbitControls(camera, renderer.domElement)
+  let stars=[];
 
   controls.enableDamping = true
   controls.dampingFactor = 0.1
+
+  function randomFloat(min, max) {
+      return Math.random() * (max - min) + min;
+  }
+  
+  function addStarField () {
+    // The loop will move from z position of -1000 to z position 1000, adding a random particle at each position.
+    for (var z = -1000; z < 1000; z += 5) {
+      // Make a sphere (exactly the same as before).
+      var geometry = new THREE.SphereGeometry(0.5, 32, 32)
+      var material = new THREE.MeshBasicMaterial({ color: 0xffffff })
+      var sphere = new THREE.Mesh(geometry, material)
+
+      // This time we give the sphere random x and y positions between -500 and 500
+      sphere.position.x = randomFloat(-1000, 1000)
+      sphere.position.y = randomFloat(-500, 500)
+
+      // Then set the z position to where it is in the loop (distance of camera)
+      sphere.position.z = z
+
+      // scale it up a bit
+      sphere.scale.x = sphere.scale.y = 2
+
+      // add the sphere to the scene
+      scene.add(sphere)
+
+      // finally push it to the stars array
+      stars.push(sphere)
+    }
+  }
 
   const play = () => {
     renderer.render(scene, camera)
@@ -27,8 +58,10 @@ export function init (container) {
   // add rootMesh to scene
   scene.add(rootMesh)
 
+  addStarField()
+
   // lighting
-  const light = new THREE.HemisphereLight(0xffffff, 0xccc, 2)
+  const light = new THREE.HemisphereLight(0xffcccc, 0xffffff, 2)
   scene.add(light)
 
   initResizeListener(container, camera, renderer)
--- src/globe/sphere.js
+++ src/globe/sphere.js
@@ -5,7 +5,7 @@ import { rootMesh } from './scene';
 const COLOR_SPHERE_NIGHT = 0xa58945;
 
 export function init() {
-  const geometry = new THREE.SphereGeometry(GLOBE_RADIUS, 40, 30);
+  const geometry = new THREE.SphereGeometry(GLOBE_RADIUS, 60, 60);
   const loader = new THREE.TextureLoader();
   const material = new THREE.MeshPhongMaterial({
     map: loader.load('../1_earth_8k.jpg'),
